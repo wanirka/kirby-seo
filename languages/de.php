3b268ccbf3aeb08cfb7f175468794e56b57dc498
Ola Christensson <ola.christensson@grandpublic.se>
2017-03-07 14:35:21 +0100

    Added German translation

---
+++ languages/de.php
@@ -0,0 +1,9 @@
+<?php
+l::set( array(
+	'seo.title' => 'SEO Titel',
+	'seo.description' => 'SEO Meta-Beschreibung',
+	'fallback' => 'Zurückfallen',
+	'values' => 'Werte',
+	'plugin.required' => 'Seo Plugin ist erforderlich!',
+	'description.fallback' => 'Dies ist eine Meta-Beschreibung, die in den Suchergebnissen erscheint. Leer lassen, damit die Suchmaschinen relevanten Text aus dem Seiteninhalt abrufen können.'
+));
